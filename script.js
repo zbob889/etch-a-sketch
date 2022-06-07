8a98bdc446ece24a5f70e6964588646db971b056
zbob889 <zroberts889@gmail.com>
2022-06-07 16:24:31 +0200

    Ugh.  Didn't get much further.  Gonna call it for today.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const grid = document.getElementById('grid');
 
 function createGrid(dimension){
-
     grid.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${dimension}, 1fr)`;
-}
+    }
