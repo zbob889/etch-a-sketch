cd30da20a1fdc1562d4075d876ccdf9cd117cf8d
zbob889 <zroberts889@gmail.com>
2022-06-07 15:15:03 +0200

    got it making a grid

---
+++ script.js
@@ -0,0 +1,7 @@
+const grid = document.getElementById('grid');
+
+function createGrid(dimension){
+
+    grid.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${dimension}, 1fr)`;
+}
