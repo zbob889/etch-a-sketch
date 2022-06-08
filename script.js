b7ebb4d0b2c204cc54a712efff5a8e949b653754
zbob889 <zroberts889@gmail.com>
2022-06-08 12:27:34 +0200

    Cleaning up

--- script.js
+++ script.js
@@ -6,10 +6,7 @@ function createGrid(dimension){
 
     for (let i = 0; i < (dimension * dimension); i++){
         let cell = document.createElement('div');
-        //cell.id = i + 1;
         cell.className = 'cell';
-        //cell.style.height = `${500/dimension}px`;
-        //cell.style.width = `${500/dimension}px`;
         cell.style.color = 'white';
         grid.appendChild(cell);
     }
