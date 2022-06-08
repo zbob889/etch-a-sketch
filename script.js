da3f855f252874d87ca7aed1a260485ae2bcb812
zbob889 <zroberts889@gmail.com>
2022-06-08 12:26:57 +0200

    Finally got the grid working.  Now I need to add hovering and such

--- script.js
+++ script.js
@@ -3,4 +3,14 @@ const grid = document.getElementById('grid');
 function createGrid(dimension){
     grid.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${dimension}, 1fr)`;
+
+    for (let i = 0; i < (dimension * dimension); i++){
+        let cell = document.createElement('div');
+        //cell.id = i + 1;
+        cell.className = 'cell';
+        //cell.style.height = `${500/dimension}px`;
+        //cell.style.width = `${500/dimension}px`;
+        cell.style.color = 'white';
+        grid.appendChild(cell);
     }
+}
